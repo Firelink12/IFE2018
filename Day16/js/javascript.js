5314a86707423d915ea924deede7902994fbe5fd
Firelink12 <34802926+Firelink12@users.noreply.github.com>
2018-05-08 16:36:50 +0800

    js

---
+++ Day16/js/javascript.js
@@ -0,0 +1,12 @@
+ var M1 = document.getElementById("more1");
+ var M2 = document.getElementById("more2");
+ var M3 = document.getElementById("more3");
+ M1.onclick = function() {
+     alert("没有什么其他的啦");
+ }
+ M2.onclick = function() {
+     alert("原专业领域内倒是有几个项目经历，但我说了你们也不了解");
+ }
+ M3.onclick = function() {
+     alert("没有更多啦，也不打算本专业继续读博了~");
+ }
